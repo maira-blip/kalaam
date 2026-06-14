f29135ce336f9d5d82807b43ac662d92bf91d1b0
maira-blip <mairazaman1212@gmail.com>
2026-06-14 20:04:08 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Kalaam from './Kalaam.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Kalaam />
+  </React.StrictMode>,
+)
